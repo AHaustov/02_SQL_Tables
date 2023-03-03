7735b9e19c72ee7b731b1c083ae92d0020af703f
AHaustov <anton.haust@gmail.com>
2023-03-03 11:51:07 +0000

    Drop_create_show_select

--- 02a_db_table_SQL.sql
+++ 02a_db_table_SQL.sql
@@ -15,3 +15,33 @@ DROP DATABASE IF EXISTS boo;
 CREATE DATABASE IF NOT EXISTS boo;
 
 SHOW DATABASES;
+
+/* Geht in die Datenbank boo */
+
+USE boo;
+
+/* Zeigt alle Tabellen in boo an */
+
+SHOW TABLES;
+
+DROP TABLE IF EXISTS coworkers;
+
+CREATE TABLE coworkers
+(
+	firstName VARCHAR(20),
+	location VARCHAR(20),
+	age INT,
+	computer VARCHAR(20)
+);
+
+SHOW TABLES;
+
+DESCRIBE coworkers;
+
+/* Dateneingabe */
+
+INSERT INTO coworkers(firstName,location,age,computer) VALUES ("Max","office",35,"PC");
+INSERT INTO coworkers(firstName,location,age,computer) VALUES ("Gani","Pool",36,"iPad");
+INSERT INTO coworkers(firstName,location,age,computer) VALUES ("Sohrab","Bett",26,"Mac");
+
+SELECT * FROM coworkers;
